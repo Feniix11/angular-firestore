d66479ee73f5a0bc39c3d51c7f2badac1bcdccc2
Felix Castro <feliixcastro11@gmail.com>
2025-03-19 09:15:25 -0600

    Instalacion y configuracion de firabase al proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductFormComponent, ProductsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-firabase';
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBjp4P0e32eTqjA5uJCIHN7B_xLBur7C7U',
+    authDomain: 'angular-firestore-e19f1.firebaseapp.com',
+    projectId: 'angular-firestore-e19f1',
+    storageBucket: 'angular-firestore-e19f1.firebasestorage.app',
+    messagingSenderId: '299230291170',
+    appId: '1:299230291170:web:52f2a97259d32e5e93eabb',
+    measurementId: 'G-R257NKPF7Q',
+  },
+};
